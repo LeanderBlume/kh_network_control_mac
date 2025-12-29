a8001c57d3144ad5de852739f746f71c22aa6c03
Leander Blume <leander@blume.ag>
2025-12-29 19:51:51 +0100

    add lo shelf and hi shelf vis
    This might suck but it's something

--- KH Volume slider/Views/EqChart.swift
+++ KH Volume slider/Views/EqChart.swift
@@ -14,7 +14,17 @@ func magnitudeResponseParametric(
     q: Double,
     f0: Double
 ) -> Double {
-    return boost * 1 / (1 + pow(q * (f / f0 - f0 / f), 2))
+    return boost / (1 + pow(q * (f / f0 - f0 / f), 2))
+}
+
+func magnitudeResponseHishelf(f: Double, boost: Double, q: Double, f0: Double) -> Double
+{
+    return boost / (1 + exp(0.01 * -q * (f - f0)))
+}
+
+func magnitudeResponseLoshelf(f: Double, boost: Double, q: Double, f0: Double) -> Double
+{
+    return boost * (1 - (1 / (1 + exp(0.01 * -q * (f - f0)))))
 }
 
 struct EqChart: View {
@@ -43,17 +53,20 @@ struct EqChart: View {
                             )
                         // Not implemented ones
                         case "LOSHELF":
-                            /*
-                            let N = 2.0 // order of filter (?)
-                            let boost = eq.boost[band]
-                            // let q = eq.q[band]
-                            // let f0 = eq.frequency[band]
-                            let fpow2N = pow(f, 2 * N)
-                            let bandResult = sqrt((fpow2N + boost * boost) / (fpow2N + 1))
-                            // result += bandResult
-                             */
-                            break
-                        case "HISHELF": break
+                            result += magnitudeResponseLoshelf(
+                                f: f,
+                                boost: eq.boost[band],
+                                q: eq.q[band],
+                                f0: eq.frequency[band]
+                            )
+
+                        case "HISHELF":
+                            result += magnitudeResponseHishelf(
+                                f: f,
+                                boost: eq.boost[band],
+                                q: eq.q[band],
+                                f0: eq.frequency[band]
+                            )
                         case "LOWPASS": break
                         case "HIGHPASS": break
                         case "BANDPASS": break
