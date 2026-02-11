d913833f6728a36712f2d67625cabef1879026b1
Leander Blume <leander@blume.ag>
2026-02-11 19:50:54 +0100

    does this break anything?

--- KH Volume slider/Sources/KHDevices.swift
+++ KH Volume slider/Sources/KHDevices.swift
@@ -139,8 +139,9 @@ final class KHDevice: @MainActor KHSingleDeviceProtocol {
                 status = .error(String(describing: error))
                 return
             }
-            state = p.copy(from: newState, into: state)
+            // state = p.copy(from: newState, into: state)
         }
+        state = newState
         status = .ready
     }
 
