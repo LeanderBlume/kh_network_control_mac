5da15da8f5339010360498496b4bafb15eedda63
Leander Blume <leander@blume.ag>
2026-02-16 15:31:03 +0100

    more status and permission stuff for release???

--- KH Volume slider/Sources/KHDevices.swift
+++ KH Volume slider/Sources/KHDevices.swift
@@ -146,18 +146,21 @@ final class KHDevice: @MainActor KHSingleDeviceProtocol {
     }
 
     func setup() async {
-        status = .busy("Setting up...")
+        status = .busy("Connecting")
         await connect()
+        status = .busy("Fetching")
         // We need to fetch product and version to identify the schema type.
         await _fetchParameters(KHParameters.setupParameters)
+        status = .busy("Loading parameters")
         await populateParameters()
         await disconnect()
         // We do NOT update the state now because that messes up the ID
     }
 
     func fetch() async {
-        status = .busy("Fetching...")
+        status = .busy("Connecting")
         await connect()
+        status = .busy("Fetching")
         await _fetchParameters(KHParameters.fetchParameters)
         await disconnect()
     }
